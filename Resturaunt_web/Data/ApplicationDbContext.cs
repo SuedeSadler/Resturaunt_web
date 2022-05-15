9e8174bd748395d3f631e81818e22127be37b779
Suede Sadler <Suede..lang.sadler@gmail.com>
2022-05-15 12:01:33 +1200

    Reservation Database added

--- Resturaunt_web/Data/ApplicationDbContext.cs
+++ Resturaunt_web/Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Resturaunt_web.Models;
 
 namespace Resturaunt_web.Data
 {
@@ -12,5 +13,6 @@ namespace Resturaunt_web.Data
             : base(options)
         {
         }
+        public DbSet<Resturaunt_web.Models.Reservations> Reservations { get; set; }
     }
 }
