7117b4035d29bb8743426a28ed590df1c3792268
Suede Sadler <Suede..lang.sadler@gmail.com>
2022-05-30 21:54:29 +1200

    Pictures added to menu

---
+++ Resturaunt_web/Data/Migrations/20220530095033_UpdateMenu.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Resturaunt_web.Data.Migrations
+{
+    public partial class UpdateMenu : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "FoodType",
+                table: "Menu",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "FoodType",
+                table: "Menu",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
