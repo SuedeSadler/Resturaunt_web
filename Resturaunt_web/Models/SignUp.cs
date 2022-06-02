69b7921947c10baec5eabb9772c958db8c3d007c
zelsha786 <84651513+zelsha786@users.noreply.github.com>
2022-06-03 10:41:01 +1200

    update

--- Resturaunt_web/Models/SignUp.cs
+++ Resturaunt_web/Models/SignUp.cs
@@ -7,5 +7,10 @@ namespace Resturaunt_web.Models
 {
     public class SignUp
     {
+        public int ID { get; set; }
+        public string EmailAddress { get; set; }
+        public string Username { get; set; }
+        public int PhoneNO { get; set; }
+        public string Password { get; set; }
     }
 }
