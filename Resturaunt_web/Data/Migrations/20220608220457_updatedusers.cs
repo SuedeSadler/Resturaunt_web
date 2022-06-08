f2dc338d6f9fa442024cce70ae068de171e57a60
ZakariyaMussa <zakariyaking04@gmail.com>
2022-06-09 10:06:09 +1200

    fixed

---
+++ Resturaunt_web/Data/Migrations/20220608220457_updatedusers.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Resturaunt_web.Data.Migrations
+{
+    public partial class updatedusers : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "Users");
+
+            migrationBuilder.RenameColumn(
+                name: "Password",
+                table: "Users",
+                newName: "LName");
+
+            migrationBuilder.RenameColumn(
+                name: "LastName",
+                table: "Users",
+                newName: "FName");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Phone",
+                table: "Users",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Date",
+                table: "Users",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<int>(
+                name: "NumPeople",
+                table: "Users",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Time",
+                table: "Users",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Date",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "NumPeople",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Time",
+                table: "Users");
+
+            migrationBuilder.RenameColumn(
+                name: "LName",
+                table: "Users",
+                newName: "Password");
+
+            migrationBuilder.RenameColumn(
+                name: "FName",
+                table: "Users",
+                newName: "LastName");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Phone",
+                table: "Users",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
