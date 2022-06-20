f95ee4acd443f4e7e03316e85b6baada127da94f
Suede Sadler <Suede.lang.sadler@gmail.com>
2022-06-21 10:10:32 +1200

    d

--- Resturaunt_web/Models/Menu.cs
+++ Resturaunt_web/Models/Menu.cs
@@ -8,11 +8,17 @@ namespace Resturaunt_web.Models
     public class Menu
     {
         [Key]
+        [Required]
         public int MenuId { get; set; }
+        
         public string Image { get; set; }
+        [Required]
         public string? Name { get; set; }
+        [Required]
         public string? Description { get; set; }
+        [Required]
         public string FoodType { get; set; }
+        [Required]
         public int Price { get; set; }
 
         [NotMapped]
